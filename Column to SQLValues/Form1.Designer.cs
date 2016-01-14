826a769ea73f75e145f93333abd6e127f79ee5a0
Роман Мельников <r.melnikau@zgs.loc>
2016-01-14 11:23:21 +0300

    1st commit

---
+++ Column to SQLValues/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Column_to_SQLValues
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(280, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(495, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Скопировать в буфер";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 45);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 305);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(262, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Вставить из буфера";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Red;
+            this.richTextBox1.Location = new System.Drawing.Point(281, 45);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(546, 305);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(781, 14);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 357);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel column to SQL insert";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+
+    }
+}
+
---
+++ Column to SQLValues/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Column_to_SQLValues
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertText()
+        {
+            int valuesInRowCount = Convert.ToInt32(numericUpDown1.Value);
+            if (valuesInRowCount < 1) return;
+            try
+            {
+                string text = textBox1.Text;
+                int totalRowCount = text.Count(p => p == '\n');
+                if (totalRowCount > valuesInRowCount)
+                {
+                    int row = 0;
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        if (text[i] == '\n')
+                        {
+                            row++;
+                            if (row % valuesInRowCount == 0)
+                            {
+                                text = text.Insert(i, "\n");
+                                i++;
+                            }
+                        }
+                    }
+                }
+                text = text.Replace(',', '.');
+                text = text.Replace("\t", "', '");
+                text = text.Replace("\r\n", "'), ('");
+                text = text.Insert(0, "('");
+                text = text.Replace(" ('\n", "\n('");
+                text = text.Remove(text.Length - 4, 4);
+                richTextBox1.Text = text;
+
+                HighLightText();
+            }
+            catch
+            {
+            }
+        }
+
+        private void HighLightText()
+        {
+            for(int i = 0; i < richTextBox1.Text.Length; i++)
+            {
+                if (richTextBox1.Text[i] == '('
+                    || richTextBox1.Text[i] == ')'
+                    || richTextBox1.Text[i] == ',')
+                {
+                    richTextBox1.Select(i, 1);
+                    richTextBox1.SelectionColor = System.Drawing.Color.Gray;
+                }
+                else
+                {
+                    richTextBox1.Select(i, 1);
+                    richTextBox1.SelectionColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            convertText();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Clipboard.GetText();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            convertText();
+        }
+    }
+}
