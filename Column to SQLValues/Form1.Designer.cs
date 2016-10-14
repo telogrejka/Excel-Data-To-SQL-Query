7606f42320594ad0318f3da1df322a9be6cfd7bc
r.melnikau <r.melnikau@e100.lt>
2016-10-14 11:20:08 +0300

    1.0.0.1

--- Column to SQLValues/Form1.Designer.cs
+++ Column to SQLValues/Form1.Designer.cs
@@ -34,6 +34,10 @@
             this.PasteFromClipboardButton = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.InsertCheckBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TableNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,9 +45,9 @@
             // 
             this.CopyToClipboardButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.CopyToClipboardButton.Location = new System.Drawing.Point(281, 12);
+            this.CopyToClipboardButton.Location = new System.Drawing.Point(280, 37);
             this.CopyToClipboardButton.Name = "CopyToClipboardButton";
-            this.CopyToClipboardButton.Size = new System.Drawing.Size(494, 23);
+            this.CopyToClipboardButton.Size = new System.Drawing.Size(547, 23);
             this.CopyToClipboardButton.TabIndex = 2;
             this.CopyToClipboardButton.Text = "Скопировать в буфер";
             this.CopyToClipboardButton.UseVisualStyleBackColor = true;
@@ -54,18 +58,18 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(12, 45);
+            this.textBox1.Location = new System.Drawing.Point(12, 66);
             this.textBox1.MaxLength = 2147483647;
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(262, 305);
+            this.textBox1.Size = new System.Drawing.Size(262, 297);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // PasteFromClipboardButton
             // 
-            this.PasteFromClipboardButton.Location = new System.Drawing.Point(12, 12);
+            this.PasteFromClipboardButton.Location = new System.Drawing.Point(12, 37);
             this.PasteFromClipboardButton.Name = "PasteFromClipboardButton";
             this.PasteFromClipboardButton.Size = new System.Drawing.Size(262, 23);
             this.PasteFromClipboardButton.TabIndex = 1;
@@ -80,31 +84,75 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.richTextBox1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.richTextBox1.Location = new System.Drawing.Point(281, 45);
+            this.richTextBox1.Location = new System.Drawing.Point(281, 66);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(546, 305);
+            this.richTextBox1.Size = new System.Drawing.Size(546, 297);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
             // 
             // numericUpDown1
             // 
             this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.numericUpDown1.Location = new System.Drawing.Point(781, 14);
+            this.numericUpDown1.Location = new System.Drawing.Point(781, 9);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
             this.numericUpDown1.TabIndex = 4;
             this.numericUpDown1.Value = new decimal(new int[] {
-            2,
+            1,
             0,
             0,
             0});
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
+            // InsertCheckBox
+            // 
+            this.InsertCheckBox.AutoSize = true;
+            this.InsertCheckBox.Location = new System.Drawing.Point(12, 9);
+            this.InsertCheckBox.Name = "InsertCheckBox";
+            this.InsertCheckBox.Size = new System.Drawing.Size(166, 17);
+            this.InsertCheckBox.TabIndex = 5;
+            this.InsertCheckBox.Text = "INSERT для каждой строки";
+            this.InsertCheckBox.UseVisualStyleBackColor = true;
+            this.InsertCheckBox.CheckedChanged += new System.EventHandler(this.InsertCheckBox_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(691, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Строк в строке";
+            // 
+            // TableNameTextBox
+            // 
+            this.TableNameTextBox.Location = new System.Drawing.Point(265, 8);
+            this.TableNameTextBox.Name = "TableNameTextBox";
+            this.TableNameTextBox.Size = new System.Drawing.Size(147, 20);
+            this.TableNameTextBox.TabIndex = 7;
+            this.TableNameTextBox.Visible = false;
+            this.TableNameTextBox.TextChanged += new System.EventHandler(this.TableNameTextBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Имя таблицы";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(839, 357);
+            this.ClientSize = new System.Drawing.Size(839, 370);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TableNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InsertCheckBox);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.PasteFromClipboardButton);
@@ -127,7 +175,10 @@
         private System.Windows.Forms.Button PasteFromClipboardButton;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
-
+        private System.Windows.Forms.CheckBox InsertCheckBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TableNameTextBox;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Column to SQLValues/Form1.cs
+++ Column to SQLValues/Form1.cs
@@ -26,23 +26,53 @@ namespace Excel_data_to_SQL_Statement
                     return;
 
                 StringBuilder text = new StringBuilder(MakeColumns(textFromClipBoard, numericUpDown1.Value));
+                text.AppendLine();
+                //if (text[text.Length-1] != '\n')
+                //    text.Append("\n");
                 text = text.Replace(',', '.');
                 text = text.Replace("\t", "', '");
-                text = text.Replace("\r\n", "'), ('");
-                text = text.Insert(0, "('");
-                text = text.Replace(" ('\n", "\n('");
+                if (InsertCheckBox.Checked)
+                {
+                    text = text.Replace("\r\n", "') ('");
+                }
+                else
+                {
+                    text = text.Replace("\r\n", "'), ('");
+                }
+                if (InsertCheckBox.Checked)
+                {
+                    text = text.Insert(0, $"INSERT INTO {TableNameTextBox.Text} VALUES ('");
+                    text = text.Replace(" ('\n", $"\nINSERT INTO {TableNameTextBox.Text} VALUES ('");
+                }
+                else
+                {
+                    text = text.Insert(0, "('");
+                    text = text.Replace(" ('\n", $"\n('");
+                }
 
                 if (linesCount > 1)
-                    text = text.Remove(text.Length - 4, 4);
+                    text = text.Remove(text.Length - 2, 2);
 
                 richTextBox1.Text = text.ToString();
+                if(!InsertCheckBox.Checked)
+                    richTextBox1.Text = richTextBox1.Text.Remove(richTextBox1.Text.LastIndexOf(','));
                 //HighLightText(); //сильно лагает на больших данных
                 Cursor.Current = Cursors.Default;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+        public string RemoveLastLine(string myStr)
+        {
+            if (myStr.Length > 2)
+            {
+                int lastNewLine;
+                if ((lastNewLine = myStr.Substring(0, myStr.Length - 2).LastIndexOf("\r\n")) != -1)
+                    return (myStr.Substring(0, lastNewLine));
+            }
+            return ("");
         }
 
         private static string MakeColumns(string text, decimal colNumber)
@@ -106,6 +136,31 @@ namespace Excel_data_to_SQL_Statement
             if (numericUpDown1.Value < 1)
             {
                 numericUpDown1.Value = 1;
+            }
+            convertText(textBox1.Text);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TableNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            convertText(textBox1.Text);
+        }
+
+        private void InsertCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if(InsertCheckBox.Checked)
+            {
+                label2.Visible = true;
+                TableNameTextBox.Visible = true;
+            }
+            else
+            {
+                label2.Visible = false;
+                TableNameTextBox.Visible = false;
             }
             convertText(textBox1.Text);
         }
